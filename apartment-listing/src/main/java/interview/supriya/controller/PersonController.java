10468ebc58449df64a9af54c909cacbbf6709fe1
supriya-rea <supriyapjoshi@hotmail.com>
2013-03-13 15:32:09 +0530

    created a multiselect option for selecting the suburbs.
    Playing with the UI. Just added some multi select box for the suburbs.

--- apartment-listing/src/main/java/interview/supriya/controller/PersonController.java
+++ apartment-listing/src/main/java/interview/supriya/controller/PersonController.java
@@ -1,5 +1,11 @@
 package interview.supriya.controller;
 
+import interview.supriya.dao.DAO;
+import interview.supriya.dao.MockDAO;
+import interview.supriya.manage.PersonManager;
+import interview.supriya.user.Listing;
+import interview.supriya.user.Person;
+
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +19,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
 
+import org.joda.time.LocalDate;
+
 @Path("create")
 public class PersonController {
 	@Context
@@ -21,14 +29,23 @@ public class PersonController {
 	@Context
 	Request request;
 	
+	
 	@POST
 	@Produces(MediaType.TEXT_HTML)
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	public void newTodo(@FormParam("email") String email,
-	      @FormParam("isForAccomodation") String isForAccomodation,
+	      @FormParam("accoCheck") String isForAccomodation,
 	      @FormParam("suburb") String suburb,
 	      @Context HttpServletResponse servletResponse) throws IOException {
-	    
+		DAO dao = new MockDAO();
+		PersonManager manager = new PersonManager(dao);
+		Person person = new Person(email);
+		Listing listing = new Listing(suburb, 0, new LocalDate());
+		person.newListing(listing);
+		manager.createNewListing(person);
+		
+	
+		
 	    
 	    servletResponse.sendRedirect("../createListing.html");
 	  }
